bc8d8493fe64e775eb6713f565ee314a2ad42aaa
tj-bu <tj-bu@DESKTOP-R346MT0>
2022-07-28 16:58:42 +0900

    두번째

--- gitTest2/src/gitTest2/gitTest2.java
+++ gitTest2/src/gitTest2/gitTest2.java
@@ -4,7 +4,7 @@ public class gitTest2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-       System.out.println("Hello JAVA 123123123123");
+       System.out.println("Hello Java");
 	}
 
 }
